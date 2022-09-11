f86eef74e04dca4e234a1b53a8e14c0f708e933f
Marilzon <maxmaril@hotmail.com>
2022-09-10 23:10:56 -0300

    FEATURE: Creating custom validators to false input

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
 const cep = document.getElementById("cep")
+const statusHandler = document.getElementById("fetch-handler-status")
+
+function clearAddressText() {
+	let logradouro = document.getElementById("logradouro")
+	let bairro = document.getElementById("bairro")
+	let localidade = document.getElementById("localidade")
+	let uf = document.getElementById("uf")
+
+	logradouro.value = ""
+	bairro.value = ""
+	localidade.value = ""
+	uf.value = ""
+
+	return
+}
 
 function dinamicCEPValues(data) {
 	for (let field in data) {
@@ -9,7 +24,7 @@ function dinamicCEPValues(data) {
 }
 
 function fetchCEP(CEPselector, DOMevent) {
-	CEPselector.addEventListener(DOMevent, (e) => {
+	CEPselector.addEventListener(DOMevent, () => {
 		let cepValue = CEPselector.value.replace("-", "")
 		const options = {
 			method: 'GET',
@@ -19,9 +34,15 @@ function fetchCEP(CEPselector, DOMevent) {
 
 		fetch(`https://viacep.com.br/ws/${cepValue}/json/`, options)
 			.then(response => {
+				statusHandler.innerText = `CEP Encontrado!`
+				statusHandler.style.color = "green"
 				response.json().then(data => dinamicCEPValues(data))
 			})
-			.catch(e => console.log(`Erro: ${e.message}`))
+			.catch(() => {
+				statusHandler.innerText = `Erro: entrada invalida!`
+				statusHandler.style.color = "red"
+				clearAddressText()
+			})
 	})
 }
 
